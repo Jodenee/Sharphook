eab44b52f1a800a5079ff72bd6dda8ad8a445efe
Jodenee <81998397+Jodenee@users.noreply.github.com>
2024-01-15 22:11:31 +0100

    Update OptionalEditMessageIcon.cs
    added new DisposeAsync util function

--- Sharphook/Models/OptionalEditMessageInfo.cs
+++ Sharphook/Models/OptionalEditMessageInfo.cs
@@ -4,13 +4,21 @@ namespace Sharphook.Models
 {
     public class OptionalEditMessageInfo
     {
-        public List<SharphookFile>? Files { get; set; }
+        public List<SharphookFile> Files { get; set; }
 
         public OptionalEditMessageInfo(
             List<SharphookFile>? files = null
         ) 
         {
-            Files = files;
+            Files = files ?? new List<SharphookFile>();
+        }
+
+        public async Task DisposeAsync()
+        {
+            foreach (SharphookFile file in Files)
+            {
+                await file.DisposeAsync();
+            }
         }
     }
 }
