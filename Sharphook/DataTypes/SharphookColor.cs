8240790649df65ee241df7092b643acb98272d10
Jodenee <81998397+Jodenee@users.noreply.github.com>
2024-01-15 22:04:56 +0100

    Update SharphookColor.cs
    Removed constructor method with no params. Removed ToRGB method, use properties Red, Green and Blue instead.

--- Sharphook/DataTypes/SharphookColor.cs
+++ Sharphook/DataTypes/SharphookColor.cs
@@ -2,9 +2,25 @@
 {
     public class SharphookColor
     {
+        public const uint MaxValue = 0xFFFFFF;
         public uint Value { get; private set; }
 
-        public SharphookColor(uint value = 0)
+        public byte Red
+        {
+            get => (byte)((Value >> 16) & 0xFF);
+        }
+
+        public byte Green
+        {
+            get => (byte)((Value >> 8) & 0xFF);
+        }
+
+        public byte Blue
+        {
+            get => (byte)((Value >> 0) & 0xFF);
+        }
+
+        public SharphookColor(uint value)
         {
             Value = value;
         }
@@ -12,28 +28,6 @@
         public SharphookColor(byte red, byte green, byte blue)
         {
             Value = Convert.ToUInt32((red << 16) + (green << 8) + blue);
-        }
-
-        public SharphookColor()
-        {
-            Random random = new Random();
-            int maxValue = 0xFFFFFF;
-
-            Value = Convert.ToUInt32(random.Next(maxValue + 1));
-        }
-
-        public void SetValue(uint value)
-        {
-            Value = value;
-        }
-
-        public (byte, byte, byte) ToRGB()
-        {
-            byte red =  (byte)((Value >> 16) & 0xFF);
-            byte green = (byte)((Value >> 8) & 0xFF);
-            byte blue = (byte)((Value >> 0) & 0xFF);
-
-            return (red, green, blue);
         }
     }
 }
