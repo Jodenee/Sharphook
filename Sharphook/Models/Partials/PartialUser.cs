7bc61cbc12aba9f691e175a25e473a3605e87316
Jodenee <81998397+Jodenee@users.noreply.github.com>
2024-02-10 23:24:16 +0100

    Update PartialUser.cs
    renamed PartialUser.Bot to PartialUser.IsBotty

--- Sharphook/Models/Partials/PartialUser.cs
+++ Sharphook/Models/Partials/PartialUser.cs
@@ -9,15 +9,15 @@ namespace Sharphook.Models
 		public string Username { get; private set; }
 		public string? AvatarHash { get; private set; }
 		public int PublicFlags { get; private set; }
-		public bool Bot { get; private set; }
+		public bool IsBot { get; private set; }
 
 		public PartialUser(WebhookClient client, PartialUserObject partialUserObject)
 		{
 			Client = client;
-			Id = Convert.ToUInt64(partialUserObject.id);
-			Username = partialUserObject.username;
-			AvatarHash = partialUserObject.avatar;
-			Bot = partialUserObject.bot ?? false;
+			Id = Convert.ToUInt64(partialUserObject.Id);
+			Username = partialUserObject.Username;
+			AvatarHash = partialUserObject.Avatar;
+            IsBot = partialUserObject.IsBot ?? false;
 		}
 
 		public string? GetAvatarUrl()
