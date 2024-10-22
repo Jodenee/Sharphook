5e4742ab4cfcc533f25eeb87192ce53ff8ef032f
Jodenee <81998397+Jodenee@users.noreply.github.com>
2024-10-22 18:32:36 +0200

    Moved EmbedFooter.cs

---
+++ Sharphook/EmbedImage.cs
@@ -0,0 +1,24 @@
+﻿using Sharphook.ResponseObjects;
+
+namespace Sharphook;
+
+public class EmbedImage
+{
+	public string Url { get; set; }
+	public string? ProxiedUrl { get; private set; }
+	public int? Width { get; private set; }
+	public int? Height { get; private set; }
+
+	public EmbedImage(string imageUrl)
+	{
+		Url = imageUrl;
+	}
+
+	internal EmbedImage(EmbedImageObject embedImageObject)
+	{
+		Url = embedImageObject.Url;
+		ProxiedUrl = embedImageObject.ProxiedUrl;
+		Width = embedImageObject.Width;
+		Height = embedImageObject.Hieght;
+	}
+}
