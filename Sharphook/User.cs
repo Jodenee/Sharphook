4ea134679945680b1d278bb9aeb988e7a724f076
Jodenee <81998397+Jodenee@users.noreply.github.com>
2024-10-22 18:40:18 +0200

    Update User.cs
    User.Avatar is now of type Asset, GetAvatarUrl has been removed

---
+++ Sharphook/User.cs
@@ -0,0 +1,34 @@
+﻿using Sharphook.ResponseObjects;
+using Sharphook.Utility.Enums;
+using Sharphook.Utility.Formatters;
+
+namespace Sharphook;
+
+public class User
+{
+	private readonly WebhookClient _client;
+	public ulong Id { get; private set; }
+	public string Username { get; private set; }
+	public string? GlobalName { get; private set; }
+	public Asset? Avatar { get; private set; }
+	public PublicUserFlag PublicFlags { get; private set; }
+	public bool IsBot { get; private set; }
+
+	public string Mention
+	{
+		get => MentionFormatter.User(Id);
+	}
+
+	internal User(WebhookClient client, UserObject userObject)
+	{
+		_client = client;
+		Id = Convert.ToUInt64(userObject.Id);
+		Username = userObject.Username;
+		GlobalName = userObject.GlobalName;
+		Avatar = userObject.Avatar != null
+			? Asset.FromAvatar(_client, Id, userObject.Avatar)
+			: null;
+		PublicFlags = userObject.PublicFlags;
+		IsBot = userObject.IsBot ?? false;
+	}
+}
