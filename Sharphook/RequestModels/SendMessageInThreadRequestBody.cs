d25774701eea1b6e74bb1bbc873373ebd753babf
Jodenee <81998397+Jodenee@users.noreply.github.com>
2024-10-22 17:57:57 +0200

    Added RequestModels

---
+++ Sharphook/RequestModels/SendMessageInThreadRequestBody.cs
@@ -0,0 +1,28 @@
+﻿using Sharphook.ResponseObjects;
+using System.Text.Json.Serialization;
+
+namespace Sharphook.RequestModels;
+
+internal sealed record SendMessageInThreadRequestBody
+{
+	[JsonPropertyName("content")]
+	public string? Content { get; set; }
+
+	[JsonPropertyName("username")]
+	public string? Username { get; set; }
+
+	[JsonPropertyName("avatar_url")]
+	public string? AvatarUrl { get; set; }
+
+	[JsonPropertyName("tts")]
+	public bool? TTS { get; set; }
+
+	[JsonPropertyName("embeds")]
+	public List<EmbedObject>? Embeds { get; set; }
+
+	[JsonPropertyName("allowed_mentions")]
+	public AllowedMentionsObject? AllowedMentions { get; set; }
+
+	[JsonPropertyName("flags")]
+	public int? Flags { get; set; }
+}
---
+++ Sharphook/RequestModels/SendMessageRequestBody.cs
@@ -0,0 +1,34 @@
+﻿using Sharphook.ResponseObjects;
+using System.Text.Json.Serialization;
+
+namespace Sharphook.RequestModels;
+
+internal sealed record SendMessageRequestBody
+{
+	[JsonPropertyName("content")]
+	public string? Content { get; set; }
+
+	[JsonPropertyName("username")]
+	public string? Username { get; set; }
+
+	[JsonPropertyName("avatar_url")]
+	public string? AvatarUrl { get; set; }
+
+	[JsonPropertyName("tts")]
+	public bool? TTS { get; set; }
+
+	[JsonPropertyName("embeds")]
+	public List<EmbedObject>? Embeds { get; set; }
+
+	[JsonPropertyName("allowed_mentions")]
+	public AllowedMentionsObject? AllowedMentions { get; set; }
+
+	[JsonPropertyName("flags")]
+	public int? Flags { get; set; }
+
+	[JsonPropertyName("thread_name")]
+	public string? ThreadName { get; set; }
+
+	[JsonPropertyName("applied_tags")]
+	public List<ulong>? AppliedTags { get; set; }
+}
