c70ed2b2b81b42e0e99ab51f16123ef9d2e76ff9
Jodenee <81998397+Jodenee@users.noreply.github.com>
2024-10-22 18:41:01 +0200

    Update Webhook.cs
    Webhook.Avatar is now of type Asset, GetAvatarUrl has been removed

---
+++ Sharphook/Webhook.cs
@@ -0,0 +1,31 @@
+﻿using Sharphook.Partials;
+using Sharphook.ResponseObjects;
+
+namespace Sharphook;
+
+public class Webhook : PartialWebhook
+{
+	public short Type { get; private set; }
+	public string Name { get; private set; }
+	public string GuildId { get; private set; }
+	public string ChannelId { get; private set; }
+	public string? ApplicationId { get; private set; }
+	public Asset? Avatar { get; private set; }
+	public User? Creator { get; private set; }
+
+	internal Webhook(WebhookClient client, WebhookObject webhookObject)
+		: base(client, Convert.ToUInt64(webhookObject.Id), webhookObject.Token)
+	{
+		Type = webhookObject.Type;
+		Name = webhookObject.Name;
+		GuildId = webhookObject.GuildId;
+		ChannelId = webhookObject.ChannelId;
+		ApplicationId = webhookObject.ApplicationId;
+		Avatar = webhookObject.AvatarHash != null
+			? Asset.FromAvatar(_client, Id, webhookObject.AvatarHash)
+			: null;
+
+		if (webhookObject.Creator != null)
+			Creator = new User(client, webhookObject.Creator);
+	}
+}
