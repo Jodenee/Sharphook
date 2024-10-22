b62a45edfa267d548226bd681ee988d71067c3de
Jodenee <81998397+Jodenee@users.noreply.github.com>
2024-10-22 18:04:58 +0200

    Update EmojiFormatter.cs
    Code style changes

--- Sharphook/Utility/Formatters/EmojiFormatter.cs
+++ Sharphook/Utility/Formatters/EmojiFormatter.cs
@@ -2,9 +2,12 @@
 
 public static class EmojiFormatter
 {
-	public static string Emoji(string emojiName) => $":{emojiName}:";
+	public static string Emoji(string emojiName)
+		=> $":{emojiName}:";
 
-	public static string CustomEmoji(string emojiName, ulong emojiId) => $"<:{emojiName}:{emojiId}>";
+	public static string CustomEmoji(string emojiName, ulong emojiId)
+		=> $"<:{emojiName}:{emojiId}>";
 
-	public static string AnimatedEmoji(string emojiName, ulong emojiId) => $"<a:{emojiName}:{emojiId}>";
+	public static string AnimatedEmoji(string emojiName, ulong emojiId)
+		=> $"<a:{emojiName}:{emojiId}>";
 }
