19df0ece343e8776c5a2fc3c3c1df9f6bd47ae89
Jodenee <81998397+Jodenee@users.noreply.github.com>
2024-10-22 18:32:18 +0200

    Moved EmbedField.cs

---
+++ Sharphook/EmbedField.cs
@@ -0,0 +1,24 @@
+﻿using Sharphook.ResponseObjects;
+
+namespace Sharphook;
+
+public class EmbedField
+{
+	public string Name { get; set; }
+	public string Value { get; set; }
+	public bool? InLine { get; set; }
+
+	public EmbedField(string fieldName, string fieldValue, bool? fieldInLine = null)
+	{
+		Name = fieldName;
+		Value = fieldValue;
+		InLine = fieldInLine;
+	}
+
+	internal EmbedField(EmbedFieldObject embedFieldObject)
+	{
+		Name = embedFieldObject.Name;
+		Value = embedFieldObject.Value;
+		InLine = embedFieldObject.Inline;
+	}
+}
