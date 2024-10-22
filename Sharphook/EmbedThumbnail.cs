cecffa203291eb0bf2a268d910632319e4e9303d
Jodenee <81998397+Jodenee@users.noreply.github.com>
2024-10-22 18:32:57 +0200

    Moved EmbedThumbnail.cs

---
+++ Sharphook/EmbedThumbnail.cs
@@ -0,0 +1,25 @@
+﻿
+using Sharphook.ResponseObjects;
+
+namespace Sharphook;
+
+public class EmbedThumbnail
+{
+	public string Url { get; set; }
+	public string? ProxiedUrl { get; private set; }
+	public int? Width { get; private set; }
+	public int? Height { get; private set; }
+
+	public EmbedThumbnail(string thumbnailUrl)
+	{
+		Url = thumbnailUrl;
+	}
+
+	internal EmbedThumbnail(EmbedThumbnailObject embedThumbnailObject)
+	{
+		Url = embedThumbnailObject.Url;
+		ProxiedUrl = embedThumbnailObject.ProxiedUrl;
+		Width = embedThumbnailObject.Width;
+		Height = embedThumbnailObject.Hieght;
+	}
+}
