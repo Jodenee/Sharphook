d8dbf0c1feb4cf5e5205e0c881ccaee800e5ffc0
Jodenee <81998397+Jodenee@users.noreply.github.com>
2024-10-22 18:45:23 +0200

    removed/moved

--- Sharphook/Models/ResponseObjects/ThreadMessageObject.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Text.Json.Serialization;
-
-namespace Sharphook.Models.ResponseObjects;
-
-public class ThreadMessageObject : MessageObject
-{
-	[JsonPropertyName("position")]
-	public uint Position { get; set; }
-}
