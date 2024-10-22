54c71ab03ce6cc9f390be751b7dc60c93e33dc42
Jodenee <81998397+Jodenee@users.noreply.github.com>
2024-10-22 18:32:04 +0200

    Moved EmbedAuthor.cs

---
+++ Sharphook/EmbedAuthor.cs
@@ -0,0 +1,26 @@
+﻿using Sharphook.ResponseObjects;
+
+namespace Sharphook;
+
+public class EmbedAuthor
+{
+	public string Name { get; set; }
+	public string? Url { get; set; }
+	public string? IconUrl { get; set; }
+	public string? ProxiedIconUrl { get; private set; }
+
+	public EmbedAuthor(string authorName, string? authorUrl = null, string? authorIconUrl = null)
+	{
+		Name = authorName;
+		Url = authorUrl;
+		IconUrl = authorIconUrl;
+	}
+
+	internal EmbedAuthor(EmbedAuthorObject embedAuthorObject)
+	{
+		Name = embedAuthorObject.Name;
+		Url = embedAuthorObject.Url;
+		IconUrl = embedAuthorObject.IconUrl;
+		ProxiedIconUrl = embedAuthorObject.ProxiedIconUrl;
+	}
+}
