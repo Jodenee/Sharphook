4119548360b61475916a313aa4c59de94592f86d
Jodenee <81998397+Jodenee@users.noreply.github.com>
2024-10-22 18:32:27 +0200

    Moved EmbedFooter.cs

---
+++ Sharphook/EmbedFooter.cs
@@ -0,0 +1,23 @@
+﻿using Sharphook.ResponseObjects;
+
+namespace Sharphook;
+
+public class EmbedFooter
+{
+	public string Text { get; set; }
+	public string? IconUrl { get; set; }
+	public string? ProxiedIconUrl { get; private set; }
+
+	public EmbedFooter(string footerText, string? footerIconUrl = null)
+	{
+		Text = footerText;
+		IconUrl = footerIconUrl;
+	}
+
+	internal EmbedFooter(EmbedFooterObject embedFooterObject)
+	{
+		Text = embedFooterObject.Text;
+		IconUrl = embedFooterObject.IconUrl;
+		ProxiedIconUrl = embedFooterObject.ProxiedIconUrl;
+	}
+}
