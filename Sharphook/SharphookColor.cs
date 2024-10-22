03f4b3b36987ff028b0196a55e30100f14b941d9
Jodenee <81998397+Jodenee@users.noreply.github.com>
2024-10-22 18:37:05 +0200

    Update SharphookColor
    Made max value private

---
+++ Sharphook/SharphookColor.cs
@@ -0,0 +1,34 @@
+﻿namespace Sharphook;
+
+public class SharphookColor
+{
+	private const uint _maxValue = 0xFFFFFF;
+	public uint Value { get; private set; }
+
+	public byte Red
+	{
+		get => (byte)(Value >> 16 & 0xFF);
+	}
+
+	public byte Green
+	{
+		get => (byte)(Value >> 8 & 0xFF);
+	}
+
+	public byte Blue
+	{
+		get => (byte)(Value >> 0 & 0xFF);
+	}
+
+	public SharphookColor(uint value)
+	{
+		if (value > _maxValue) throw new ArgumentOutOfRangeException($"value needs to be less than or equal to {_maxValue}!");
+
+		Value = value;
+	}
+
+	public SharphookColor(byte red, byte green, byte blue)
+	{
+		Value = Convert.ToUInt32((red << 16) + (green << 8) + blue);
+	}
+}
