5145d31c16fec95d53548f7f5ae49dd1befb5e44
Jodenee <81998397+Jodenee@users.noreply.github.com>
2024-10-22 18:37:58 +0200

    Updated SharphookFile.cs
    minor code improvements

---
+++ Sharphook/SharphookFile.cs
@@ -0,0 +1,50 @@
+﻿namespace Sharphook;
+
+public class SharphookFile
+{
+	private readonly string _filename;
+
+	public string FilePath;
+	public bool Spoiler { get; private set; }
+	public string? Description { get; private set; }
+	public string FileName
+	{
+		get => Spoiler ? $"SPOILER_{_filename}" : _filename;
+	}
+	public string AttachmentLink
+	{
+		get => $"attachment://{FileName}";
+	}
+	public FileStream Stream
+	{
+		get => File.OpenRead(FilePath);
+	}
+
+	public SharphookFile(string filepath, string? filename = null, bool spoiler = false, string? description = null)
+	{
+		FilePath = filepath;
+		Spoiler = spoiler;
+		Description = description;
+
+		_filename = filename ?? Path.GetFileName(FilePath);
+	}
+
+	public SharphookFile(FileStream fileStream, string? filename = null, bool spoiler = false, string? description = null)
+	{
+		FilePath = fileStream.Name;
+		Spoiler = spoiler;
+		Description = description;
+
+		_filename = filename ?? Path.GetFileName(fileStream.Name);
+	}
+
+	internal object ToObject(int index)
+	{
+		return new
+		{
+			id = index,
+			filename = FileName,
+			description = Description,
+		};
+	}
+}
