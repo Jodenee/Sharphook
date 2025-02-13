77a6fa7b6a4e35118ecdd58de8d86108598a71a2
Jodene Borg <81998397+Jodenee@users.noreply.github.com>
2025-02-13 12:52:54 +0100

    added PollPartialEmojiObject.cs

---
+++ Sharphook/ResponseModels/PollPartialEmojiObject.cs
@@ -0,0 +1,14 @@
+﻿#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
+using System.Text.Json.Serialization;
+
+namespace Sharphook.ResponseModels;
+
+internal sealed record PollPartialEmojiObject
+{
+    [JsonPropertyName("id")]
+    public ulong? Id { get; set; }
+
+    [JsonPropertyName("name")]
+    public string? Name { get; set; }
+}
