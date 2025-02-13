7b5e6757e6d5a70fac78131dcad2a0ffcb23aea0
Jodene Borg <81998397+Jodenee@users.noreply.github.com>
2025-02-13 12:51:04 +0100

    added PollMediaObject.cs

---
+++ Sharphook/ResponseModels/PollMediaObject.cs
@@ -0,0 +1,14 @@
+﻿#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
+using System.Text.Json.Serialization;
+
+namespace Sharphook.ResponseModels;
+
+internal sealed record PollMediaObject
+{
+    [JsonPropertyName("text")]
+    public string? Text { get; set; }
+
+    [JsonPropertyName("emoji")]
+    public PollPartialEmojiObject? Emoji { get; set; }
+}
