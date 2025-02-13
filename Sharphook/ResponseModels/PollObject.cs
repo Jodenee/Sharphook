19955fd7db1a421e68e3bf55d3a569c543b9a37c
Jodene Borg <81998397+Jodenee@users.noreply.github.com>
2025-02-13 12:54:07 +0100

    added PollObject.cs

---
+++ Sharphook/ResponseModels/PollObject.cs
@@ -0,0 +1,26 @@
+﻿#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
+using System.Text.Json.Serialization;
+
+namespace Sharphook.ResponseModels;
+
+internal sealed record PollObject
+{
+    [JsonPropertyName("question")]
+    public PollMediaObject Question { get; set; }
+
+    [JsonPropertyName("answers")]
+    public List<PollAnswerObject> Answers { get; set; }
+
+    [JsonPropertyName("expiry")]
+    public string Expiry { get; set; }
+
+    [JsonPropertyName("allow_multiselect")]
+    public bool AllowMultiselect { get; set; }
+
+    [JsonPropertyName("layout_type")]
+    public string LayoutType { get; set; }
+
+    [JsonPropertyName("results")]
+    public PollResultsObject? Results { get; set; }
+}
