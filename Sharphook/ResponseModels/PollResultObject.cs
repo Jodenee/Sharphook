44e3edb4f9b84b4aa339e89aea37e5263df12049
Jodene Borg <81998397+Jodenee@users.noreply.github.com>
2025-02-13 12:53:41 +0100

    added PollResultObject.cs

---
+++ Sharphook/ResponseModels/PollResultObject.cs
@@ -0,0 +1,17 @@
+﻿#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
+using System.Text.Json.Serialization;
+
+namespace Sharphook.ResponseModels;
+
+internal class PollResultObject
+{
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+
+    [JsonPropertyName("count")]
+    public int Votes { get; set; }
+
+    [JsonPropertyName("me_voted")]
+    public bool IVoted { get; set; }
+}
