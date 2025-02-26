f53daffe00b940de040b34e0ead7ce73d2334822
Jodenee <81998397+Jodenee@users.noreply.github.com>
2025-02-26 13:29:52 +0100

    Update CreatePoll.cs
    No longer creates a list by default for no reason, also changed the constructor to only take the question because an emoji is not allowed in the question field for some reason even though it's a poll media object

--- Sharphook/CreatePoll.cs
+++ Sharphook/CreatePoll.cs
@@ -5,19 +5,19 @@ namespace Sharphook;
 public class CreatePoll
 {
     public PollMedia Question { get; set; }
-    public List<PollAnswer> Answers { get; set; } = new List<PollAnswer>();
+    public List<PollAnswer> Answers { get; set; }
     public int Duration { get; set; }
     public bool AllowMultipleChoice { get; set; }
     public PollLayoutType LayoutType { get; set; }
 
     public CreatePoll(
-        PollMedia question, 
+        string question, 
         List<PollAnswer> answers,
 		int duration,
 		bool allowMultipleChoice = false,
 		PollLayoutType layoutType = PollLayoutType.DEFAULT)
     {
-        Question = question;
+        Question = new PollMedia(question);
         Answers = answers;
 		Duration = duration;
         AllowMultipleChoice = allowMultipleChoice;
