f7c74ffb0657e39970ef05ef2a028d552d996d1e
Jodenee <81998397+Jodenee@users.noreply.github.com>
2025-04-13 23:52:11 +0200

    update Webhook.cs
    GuildId, ChannelId and ApplicationId are now all of type ulong

--- Sharphook/Webhook.cs
+++ Sharphook/Webhook.cs
@@ -5,11 +5,11 @@ namespace Sharphook;
 
 public sealed class Webhook : PartialWebhook
 {
-	public short Type { get; private set; }
+	public int Type { get; private set; }
 	public string Name { get; private set; }
-	public string GuildId { get; private set; }
-	public string ChannelId { get; private set; }
-	public string? ApplicationId { get; private set; }
+	public ulong GuildId { get; private set; }
+	public ulong ChannelId { get; private set; }
+	public ulong? ApplicationId { get; private set; }
 	public Asset? Avatar { get; private set; }
 	public User? Creator { get; private set; }
 
@@ -18,14 +18,16 @@ public sealed class Webhook : PartialWebhook
 	{
 		Type = webhookObject.Type;
 		Name = webhookObject.Name;
-		GuildId = webhookObject.GuildId;
-		ChannelId = webhookObject.ChannelId;
-		ApplicationId = webhookObject.ApplicationId;
+		GuildId = Convert.ToUInt64(webhookObject.GuildId);
+		ChannelId = Convert.ToUInt64(webhookObject.ChannelId);
+		ApplicationId = webhookObject.ApplicationId != null 
+			? Convert.ToUInt64(webhookObject.ApplicationId) 
+			: null;
 		Avatar = webhookObject.AvatarHash != null
-			? Asset.FromAvatar(_client, Id, webhookObject.AvatarHash)
+			? Asset.FromAvatar(this, Id, webhookObject.AvatarHash)
 			: null;
 
 		if (webhookObject.Creator != null)
-			Creator = new User(client, webhookObject.Creator);
+			Creator = new User(this, webhookObject.Creator);
 	}
 }
