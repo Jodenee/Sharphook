d7a1cd365e016a1ffc92ca602ea474b5552d5970
Jodenee <81998397+Jodenee@users.noreply.github.com>
2025-06-18 08:51:57 +0200

    Update CreatePoll.cs
    added validation for incorrect answer array sizes

--- Sharphook/CreatePoll.cs
+++ Sharphook/CreatePoll.cs
@@ -5,18 +5,23 @@ namespace Sharphook;
 public class CreatePoll
 {
     public PollMedia Question { get; set; }
-    public List<PollAnswer> Answers { get; set; }
+    public PollAnswer[] Answers { get; set; }
     public int Duration { get; set; }
     public bool AllowMultipleChoice { get; set; }
     public PollLayoutType LayoutType { get; set; }
 
     public CreatePoll(
-        string question, 
-        List<PollAnswer> answers,
-		int duration,
+        string question,
+        PollAnswer[] answers,
+        int duration,
 		bool allowMultipleChoice = false,
 		PollLayoutType layoutType = PollLayoutType.DEFAULT)
     {
+        if (answers.Length > 10) 
+            throw new ArgumentException("A poll cannot have more than 10 answers.", nameof(answers));
+        else if (answers.Length == 0)
+            throw new ArgumentException("A poll must have at least 1 answer.", nameof(answers));
+
         Question = new PollMedia(question);
         Answers = answers;
 		Duration = duration;
