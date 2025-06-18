de57a1b256c236ef63f46d6200fc1f2a069eef46
Jodenee <81998397+Jodenee@users.noreply.github.com>
2025-06-18 08:50:37 +0200

    Update Poll.cs
    Removed unused constructor method, Answers is no longer assigned as an array of PollAnswer of size 10 at creation as in cases where you would have less than 10 answers you would have null elements

--- Sharphook/Poll.cs
+++ Sharphook/Poll.cs
@@ -6,34 +6,22 @@ namespace Sharphook;
 public class Poll
 {
     public PollMedia Question { get; set; }
-    public List<PollAnswer> Answers { get; set; } = new List<PollAnswer>();
+    public PollAnswer[] Answers { get; set; }
     public DateTime ExpiresAt { get; set; }
     public bool AllowMultipleChoice { get; set; }
     public PollLayoutType LayoutType { get; set; }
     public PollResults? Results { get; set; }
 
-    public Poll(PollMedia question, 
-        List<PollAnswer> answers, 
-        TimeSpan expiresIn, 
-        bool allowMultipleChoice, 
-        PollLayoutType? layoutType = null)
-    {
-        Question = question;
-        Answers = answers;
-        ExpiresAt = DateTime.Now + expiresIn;
-        AllowMultipleChoice = allowMultipleChoice;
-        LayoutType = layoutType ?? PollLayoutType.DEFAULT;
-    }
-
     internal Poll(PollObject pollObject)
     {
         Question = new PollMedia(pollObject.Question);
+        Answers = new PollAnswer[pollObject.Answers.Length];
         ExpiresAt = DateTime.Parse(pollObject.Expiry);
         AllowMultipleChoice = pollObject.AllowMultiselect;
         LayoutType = (PollLayoutType) pollObject.LayoutType;
         Results = pollObject.Results != null ? new PollResults(pollObject.Results) : null;
 
-        foreach (PollAnswerObject answerObject in pollObject.Answers)
-            Answers.Add(new PollAnswer(answerObject));
+        for (int i = 0; i < pollObject.Answers.Length; i++)
+            Answers[i] = new PollAnswer(pollObject.Answers[i]);
     }
 }
